e347db467a0932c83875e0232e614d953945be95
jaffleman <jaffleman@gmail.com>
2022-08-06 21:35:50 +0200

    creation de la modale

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { Button, StyleSheet, View, Text, Dimensions, TouchableOpacity, Modal, Pressable, TextInput } from 'react-native';
+import {  StyleSheet, View, Text, Dimensions} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import MapView,  { Marker, PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import requestLocationPermission from './src/requestLocationPermission'
-import { FAB } from 'react-native-paper';
+import { Button, FAB, Modal, TextInput, Portal } from 'react-native-paper';
 
 const {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -12,7 +12,9 @@ const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default app=() => {
   console.log('STARTER')
+  const [showModal,setShowModal] = useState(false)
   const [coordonates, setCoordonates]= useState({"permission":false})
+  let code, accesType
   function getPosition(callback) {
     Geolocation.getCurrentPosition(({coords})=>{
       callback(coords)
@@ -22,6 +24,9 @@ export default app=() => {
     })
   }
 
+  function modalswitcher(){
+    setShowModal(!showModal)
+  }
   useEffect(()=>{
     requestLocationPermission((permission)=>{
       if(permission){
@@ -53,9 +58,48 @@ export default app=() => {
           <FAB
             icon="plus"
             style={styles.fab}
-            onPress={() => console.log('Pressed')}
+            onPress={modalswitcher}
           />
+            <Portal>
+          <Modal visible={showModal} onDismiss={modalswitcher} style={{
+             height,
+             width,
+             position: 'absolute',
+             top: 0,
+             left: 0,
+             justifyContent: 'center',
+          } }
+          contentContainerStyle={{
+            top: 0,
+            left: 0,
+            backgroundColor:'white', 
+            borderRadius:10,
+            position: 'absolute',
+            // justifyContent: 'center',
+          }} >
+              <Text style={{textAlign:'center', margin:10}}>le text de ma modale </Text>
+              <TextInput
+                style={{width:350, marginLeft:'auto', marginRight:'auto'}}
+                autoCapitalize
+                placeholder='Hall, portail, ascenseur, escalier...'
+                label="Type d'acces"
+                left={<TextInput.Icon name="boom-gate"/>}
+                onChangeText={(e)=>accesType=e}
+                
+              />
+              <TextInput
+                autoCapitalize='characters'
+                style={{width:350, marginLeft:'auto', marginRight:'auto'}}
+                label="CODE"
+                left={<TextInput.Icon name="lock" />}
+                onChangeText={(e)=>code=e}
+              />
+              <Button mode='containedtext' onPress={modalswitcher}>close</Button>
+              <Button mode='containedtext' onPress={()=>console.log(accesType,code)}>valide</Button>
+          </Modal>
+            </Portal>
         </View>
+        
   }else {
     return(
       <Button title='authorisation' 
